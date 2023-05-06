2dadd012bb4eed9cb716de2b8d96373cc1de3747
noor uddin muhammad faateh <52114434+faateh77@users.noreply.github.com>
2023-05-06 08:48:23 +0500

    Add files via upload

---
+++ chapter 21 to 25/Chapter 4/Chapter4Ques2.js
@@ -0,0 +1,17 @@
+/*legal Variables*/
+var name = "John";
+var _name = "Asif";
+var firstName = "Joe";
+var string_122 = "Hello World";
+var NAME  = "JOHN";
+document.write(name + "<br>" + _name + "<br>" + firstName + "<br>" + string_122 + "<br>" + NAME);
+
+/*Illegal Variables*/
+
+// Note: This will not print so this illegal  variables are commented for display
+
+// var 22name = "John";
+// var @name = "Asif";
+// var = "Joe";
+// var for  = "Hello World";
+// var 12$name = "adnan";
