a10e8b181dfac82b203b849cbd68a2c159d8b036
noor uddin muhammad faateh <52114434+faateh77@users.noreply.github.com>
2023-05-06 08:51:36 +0500

    Add files via upload

---
+++ Chapter 4/Chapter4Ques1.js
@@ -0,0 +1,2 @@
+var name = "joe" , age = 34 , profession = "Doctor" ;
+document.write("My name is" + name + " " + "I am" + " " + age + " " + "years old and i am a " + profession);
---
+++ Chapter 4/Chapter4Ques3.js
@@ -0,0 +1,7 @@
+/*Rules for naming JS Variables*/ 
+document.write("3.a) A Heading Stating JS Varibales" + "<br>");
+document.write("3.b) Varibale names can only contain number, $ and _ , For Example $my_1stVariable" + "<br>");
+document.write("3.c) A Variable must begin with a letter, $ or _ For Example <h3>$name,_name</h3> or <h3>name</h3>"   + "<br>");
+document.write("3.d) Variable names are case sensitive" + "<br>");
+document.write("3.e) Variable names should not be JS Keywords" + "<br>");
+ 
